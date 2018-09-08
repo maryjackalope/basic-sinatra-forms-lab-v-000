72878fdd30743893cd4bd9602599dcd98bdfd05a
Mary Jack <maryjackinthebox@gmail.com>
2018-09-08 01:36:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,17 +2,14 @@ require 'sinatra/base'
 
 
 class App < Sinatra::Base
-  get '/reverse' do
-    erb :reverse
-  end
-  
-  post '/reverse' do
+
+  post '/team' do
     original_string = params["string"]
     @reversed_string = original_string.reverse
-    erb :reversed	   
+    erb :team	   
   end	  
   
-  get '/friends' do
+  get '/' do
     # Write your code here!	    
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
      erb :friends
